0feac40f20559b1fee9431f5724f8c0276784530
Jayden Pham <jaydenphamofficial@gmail.com>
2025-06-22 15:13:44 +1200

    UPDATE: Added profile image to about me page

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,3 +1,4 @@
+import profileImage from '../../assets/images/me.png';;
 
 export const About = () => {
     const frontendSkills =["React", "TailwindCSS", "HTML5", "CSS3"]
@@ -6,9 +7,14 @@ export const About = () => {
     return (
         <section id="about" className="flex items-center justify-center py-20">
             <div className="max-w-3xl mx-auto px-4">
-                <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent text-center">
+                <div className='flex justify-center mb-4'>
+                    <img className="w-50 h-50 rounded-full" src={profileImage} alt="Profile Image"/>
+                </div>
+
+                <h2 className="text-3xl font-bold mb-4 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent text-center">
                     About Me
                 </h2>
+            
 
                 {/* Small intro about me section*/}
                 <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
