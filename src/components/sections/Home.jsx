52678ff482ab8e18ab50756a17c5094b1846b728
Jayden Pham <jaydenphamofficial@gmail.com>
2025-06-23 20:54:27 +1200

    Update Home.jsx

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -20,7 +20,7 @@ export const Home = () => {
                 <h1 className="text-center text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right"> 
                     Hi, I'm Jayden Pham
                 </h1>
-                <p className ="text-gray-400 text-lg mb-8 max-w-lg mx-auto flex justify-center">
+                <p className ="text-center text-gray-400 text-lg mb-8 max-w-lg mx-auto flex justify-center">
                     A final year CompSci student at the University of Auckland.
                 </p>
 
