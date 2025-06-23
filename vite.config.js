68e42c284b64f8fba40717b57d6792da6bc9c9a8
Jayden Pham <jaydenphamofficial@gmail.com>
2025-06-23 20:00:21 +1200

    updated configs

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/jaydenpham-portfolio",
+  base: "/self-portfolio-project-2025",
 });
