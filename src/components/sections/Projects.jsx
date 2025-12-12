45ac59929c821f245098914e4baae4ba8fea5d5d
Jayden Pham <jaydenphamofficial@gmail.com>
2025-12-12 20:46:16 +1300

    UPDATE: padding

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -27,7 +27,7 @@ export const Projects = () => {
     }, []);
 
     return (
-        <section id="projects" className="min-h-screen flex items-center justify-center py-100">
+        <section id="projects" className="min-h-screen flex items-center justify-center py-20">
             <RevealOnScroll>
                 <div className="max-w-5xl mx-auto px-4">
                     <h2 className="text-3xl mb-2 font-bold bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent text-center">Featured Projects</h2>
